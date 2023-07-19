bfc70c0fbb3a7de348e27fe09ad269a885d7b817
penekjd <greedan@mail.ru>
2023-07-19 18:38:30 +0300

    Add class-validator for incoming data in product.dto

--- nestjs/src/product/dto/create-product.dto.ts
+++ nestjs/src/product/dto/create-product.dto.ts
@@ -1,5 +1,23 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+
 export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
   readonly title: string
+
+  @IsOptional() 
+  @IsString()
   readonly desc: string
+
+  @IsNotEmpty()
+  @IsNumber()
   readonly ser: number
+
+  //Also you can use IsEnum if you want recive complex data as object
+  //EXAMPLE:
+  /* 
+    @IsNotEmpty()
+    @IsEnum(SomeClass, { message: 'Message if 400 error' })
+    readonly someObject: SomeClass
+  */
 }
